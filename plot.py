de9d87574441cee3bf2e2845160fa8d8e9761672
João Martins <joaolucassilvamartins@gmail.com>
2021-11-07 23:58:39 +0000

    Update: Plots

--- plot.py
+++ plot.py
@@ -4,102 +4,130 @@ from numpy import NaN
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sqlalchemy import create_engine
+import numpy as np
+import datetime
 
-# Load File
-f = open('stories.json',)
-data = json.load(f)
-df = pd.DataFrame.from_dict(data, orient='columns')
+# Some base vars
+color = "orange"
+plt.rcParams['font.size'] = 16
+plt.rcParams['figure.autolayout'] = True
 
-# Set NaN to dead=false
-df["dead"].fillna(False, inplace=True)
+
+# Load Database
+engine = create_engine("sqlite:///data.db", echo=False)
+stories_df = pd.read_sql_query(
+    '''SELECT * FROM Story JOIN Type ON(story_type = type_id)''', con=engine)
+
+comments_df = pd.read_sql_query(
+    '''SELECT * FROM Comment''', con=engine)
 
 # Convert Unix timestamp to date
-df["time"] = pd.to_datetime(df['time'], unit='s')
-
-
-def groupby_month(df):
-    return df.groupby(df['time'].dt.month)
-
-
-def groupby_year(df):
-    return df.groupby(df['time'].dt.year)
-
-
-def groupby_trimester(df):
-    return df.groupby(pd.Grouper(key='time', freq='3M'))
-
-
-def groupby_hour(df):
-    return df.groupby(df['time'].dt.hour)
-
-
-def groupby_author(df):
-    return df.groupby(df['by'])
-
-
-def plot_top_count(df, N=50):
-    top50 = df['id'].count().sort_values(ascending=False).head(N)
-    top50.plot(kind="bar")
-    plt.show()
-
-
-def plot_top_posts_by_comment(df, N=50):
-    df['kids'] = df['kids'].apply(len)
-    top50 = df.sort_values(['kids'], ascending=False).head(N)
-    print(top50.head())
-    top50.plot(kind="bar", x="id", y="kids")
-    plt.show()
-
-
-def plot_top_posts_by_score(df, N=50):
-    top50 = df.sort_values(['score'], ascending=False).head(N)
-    top50.plot(kind="bar", x="id", y="score")
-    plt.show()
-
-
-def plot_count(df):
-    df['id'].count().plot(kind="line")
-    plt.show()
-
-
-def plot_score(df):
-    df['score'].mean().plot(kind="bar")
-    plt.show()
-
-
-def plot_dead(df):
-    df.groupby("dead").size().plot(kind="bar")
-    plt.show()
+stories_df["story_time"] = pd.to_datetime(stories_df['story_time'], unit='s')
 
 
 def plot_heatmap_score(df):
-    median_score = df.groupby([df['time'].dt.hour, df['time'].dt.weekday])[
-        'score'].mean().rename_axis(['hour', 'day']).reset_index()
+    median_score = df.groupby([df['story_time'].dt.hour, df['story_time'].dt.weekday])[
+        'story_score'].mean().rename_axis(['hour', 'day']).reset_index()
     print(median_score)
     median_score = median_score.pivot(
-        index='hour', columns='day', values='score')
+        index='hour', columns='day', values='story_score')
     sns.heatmap(median_score, annot=True, fmt="g", cmap='viridis',
                 xticklabels=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"])
     plt.show()
+plot_heatmap_score(stories_df)
 
 
-def score_box_plot(df):  # TODO maybe pass groupby by arg
-    groupby_year(df).boxplot(subplots=False, rot=45, column=["score"])
+# Numero de posts/score por mes ou hora ou dia
+def posts_per_time(df):
+    plt.rcParams['font.size'] = 12
+    months = {i: datetime.date(2000, i, 1).strftime('%b')
+              for i in range(1, 13)}
+    days = {i-3: datetime.date(2000, 1, i).strftime('%A')
+            for i in range(3, 10)}
+
+    plt.subplot(2, 2, 1)
+    month_plot = df.groupby(df['story_time'].dt.month).size().rename(
+        months).plot(kind="bar", rot=0, xlabel="Months", ylabel="Number of Stories", color=color)
+
+    plt.subplot(2, 2, 2)
+    days_plot = df.groupby(df['story_time'].dt.weekday).size().rename(
+        days).plot(kind="bar", rot=0, xlabel="Days", ylabel="Number of Stories", color=color)
+
+    plt.subplot(2, 1, 2)
+    hour_plot = df.groupby(df['story_time'].dt.hour).size().plot(
+        kind="bar", rot=0, xlabel="Hour", ylabel="Number of Stories", color=color)
+
+    plt.show()
+# posts_per_time(stories_df)
+
+# + bar plot com tipo de posts
+
+
+def posts_per_type(df):
+    bars = df['type_name'].value_counts().plot.bar(
+        rot=0, color=color, xlabel="Type", ylabel="Number of Stories")
+    for p in bars.patches:
+        bars.annotate(str(p.get_height()), (p.get_x() + 0.50 *
+                      p.get_width(), p.get_y() + p.get_height() + 300.5), va="center", ha="center")
     plt.show()
 
 
-def comment_box_plot(df, groupby):
-    df['kids'] = df['kids'].apply(len)  # TODO prob do always this?
-    groupby(df).boxplot(subplots=False, rot=45, column=["kids"])
+# posts_per_type(stories_df)
+
+# + Stories com e sem texto no body
+# posts_per_havingtext(stories_df)
+
+# + Numero de descendentes por posts
+
+
+def descendants_per_type(df):  # TODO maybe pass groupby by arg
+    df.columns = df.columns.str.replace('type_name', 'Type')
+    df.columns = df.columns.str.replace(
+        'story_descendants', 'Number of Comments')
+    sns.violinplot(data=df, x="Type", y="Number of Comments")
     plt.show()
 
 
-# plot_dead(df)
-# groupby_trimester(df)['id'].count().plot(kind="line")
-# plot_count(groupby_trimester(df))
-# plot_top_count(groupby_author(df))
-# plot_top_posts_by_score(df)
-# plot_top_posts_by_comment(df)
-# plot_score(groupby_hour(df))
-# plot_heatmap_score(df)
-comment_box_plot(df, groupby_month)
+# descendants_per_type(stories_df)
+
+
+# + Numero de descendentes por score
+
+def score_per_descendants(df):  # TODO maybe pass groupby by arg
+    # Done with bar plot
+    # desc_min, desc_max = df['story_descendants'].min(
+    # ), df['story_descendants'].max()
+    # steps = 100
+    # bins = [i for i in range(desc_min, desc_max + 1,
+                             # (desc_max - desc_min) // steps)]
+    # df = df.groupby(pd.cut(df['story_descendants'], bins=bins))[
+        # 'story_score'].median()
+    # df.plot.bar()
+
+    # Done with line plot
+    plot = df.groupby(df['story_descendants'])['story_score'].median().plot(color=color)
+    plot.set_xlabel("Number of Descendants")
+    plot.set_ylabel("Score")
+    plt.show()
+
+
+# score_per_descendants(stories_df)
+# + Estimativa de numero total de comments
+# Stories têm numero total de comments dela
+# Calcular percentagem de comments que pertencem ao primeiro comment
+
+def comment_percentage(story_df, comment_df):
+    #print(comment_df)
+    def get_percentage(tup):
+        comment = comment_df[comment_df["comment_parent"] == tup['story_id']]
+
+        if (len(comment) > 0):
+            comment_descendants = comment.iloc[0]['comment_descendants']
+            story_descendants = tup['story_descendants']
+            return comment_descendants / story_descendants
+        else:
+            return 0
+    story_df['top_comment_perc'] = story_df.apply(get_percentage, axis=1)
+    print(story_df['top_comment_perc'].median())
+comment_percentage(stories_df, comments_df)
