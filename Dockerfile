b63261a1fa033e4402b3f622aad7159ea2d438fd
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-12-01 19:57:31 +0000

    Part of schema

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,8 @@ FROM solr:8.10
 
 COPY hackersearch.json /data/hackersearch.json
 
-#COPY simple_schema.json /data/simple_schema.json
-
 COPY startup.sh /scripts/startup.sh
 
-EXPOSE 8983
+COPY hackersearch_schema.json /data/hackersearch_schema.json
 
 ENTRYPOINT ["/scripts/startup.sh"]
--- startup.sh
+++ startup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-precreate-core hackersearch
+#precreate-core hackersearch
 
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
 
+solr create -c hackersearch
+
 # Schema definition via API
-#curl -X POST -H 'Content-type:application/json' \
-#    --data-binary @/data/simple_schema.json \
-#    http://localhost:8983/solr/courses/schema
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @/data/hackersearch_schema.json \
+    http://localhost:8983/solr/hackersearch/schema
 
 # Populate collection
 bin/post -c hackersearch /data/hackersearch.json
